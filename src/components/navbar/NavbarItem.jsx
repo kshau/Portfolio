88db89374bd9fc34080f10f01ee6c574cf00eb41
Shaurya Kumar <Shaurya>
2024-03-19 07:43:46 -0400

    test

--- src/components/navbar/NavbarItem.jsx
+++ src/components/navbar/NavbarItem.jsx
@@ -7,7 +7,7 @@ function NavbarItem(props) {
             <a href={href} className={`flex flex-row mx-[30px] ${(name == active) ? ("underline") : ("")} decoration-navbar-item-underline underline-offset-[6px]`}>
                 <img src={icon} className="w-[60px] fill-white"/>
                 <span className="text-white text-[40px] mt-[30px] font-bold ml-[15px]">{name}</span>
-            </a>
+            </a >
         </>
     )
 
