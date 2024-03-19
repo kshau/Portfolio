f3188de96810398906430eff17323e3eac28ab81
Shaurya Kumar <kshau@Shauryas-MacBook-Air.local>
2024-03-19 13:40:31 -0400

    work

--- src/components/footer/SocialMediaLink.jsx
+++ src/components/footer/SocialMediaLink.jsx
@@ -4,7 +4,7 @@ function SocialMediaLink(props) {
 
     return (
         <>
-            <a className="flex flex-row text-white font-bold bg-social-link-bg w-fit h-fit rounded-[30px] p-[15px] mx-[7px] mt-[2vh]" href={href} target="_blank">
+            <a className="flex flex-row text-white font-bold bg-social-link-bg w-fit h-fit rounded-[30px] p-[15px] mx-[15px] mt-[2vh] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={href} target="_blank">
                 <img src={icon} className="w-[60px]"/>
                 <span className="ml-[15px] text-[40px]">{handle}</span>
             </a>
--- src/components/navbar/NavbarItem.jsx
+++ src/components/navbar/NavbarItem.jsx
@@ -4,10 +4,10 @@ function NavbarItem(props) {
 
     return (
         <>
-            <a href={href} className={`flex flex-row mx-[30px] ${(name == active) ? ("underline") : ("")} decoration-navbar-item-underline underline-offset-[6px]`}>
-                <img src={icon} className="w-[60px] fill-white"/>
-                <span className="text-white text-[40px] mt-[30px] font-bold ml-[15px]">{name}</span>
-            </a >
+            <a href={href} className={`flex flex-row mx-[30px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 decoration-navbar-item-underline underline-offset-[6px]`}>
+                <img src={icon} className="w-[60px]"/>
+                <span className={`text-[40px] mt-[30px] font-bold ml-[15px] ${(name == active) ? ("text-white") : ("text-gray-500")}`}>{name}</span>
+            </a>
         </>
     )
 
