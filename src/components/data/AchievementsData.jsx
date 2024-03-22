53eec7104e5e107d2de94ef962abb2b03ba8b360
Shaurya Kumar <Shaurya>
2024-03-22 18:48:38 -0400

    updated achievements

--- src/components/data/AchievementsData.jsx
+++ src/components/data/AchievementsData.jsx
@@ -1,5 +1,7 @@
 import Image_GaSTC2024 from "../../assets/achievements/gastc_2024.png";
 import Image_LanierTechFair2024 from "../../assets/achievements/lanier_tech_fair_2024.png";
+import Image_HackGwinett from "../../assets/achievements/hack_gwinett.png";
+import Image_JSEEntryLvl from "../../assets/achievements/jse_entry_lvl.png";
 
 const AchievementsData = [
 
@@ -7,14 +9,14 @@ const AchievementsData = [
         "event": "JSE Certification", 
         "award": "Certified Entry-Level JS Programmer",
         "date": "July 30th, 2023",
-        "image": Image_LanierTechFair2024, 
+        "image": Image_JSEEntryLvl, 
     }, 
 
     {
         "event": "HackGwinett 3 (2023)", 
         "award": "Best Humanitarian",
         "date": "October 28th, 2023",
-        "image": Image_LanierTechFair2024, 
+        "image": Image_HackGwinett, 
     }, 
 
     {
