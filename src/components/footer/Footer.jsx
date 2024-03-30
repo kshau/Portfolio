07c79c4e30d9b0dbba2a5c1bee9051d5095c5ad0
Shaurya Kumar <63877755+kshau@users.noreply.github.com>
2024-03-29 23:46:44 -0400

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ function Footer() {
     return (
         <>
             <div className="bg-footer-bg w-[100vw] h-[85px] fixed bottom-0 flex justify-center">
-                <SocialMediaLink handle="@kshau" icon={DiscordLogo}/>
+                <SocialMediaLink handle="@kshau" icon={DiscordLogo} href="https://discordapp.com/users/768181277814685706"/>
                 <SocialMediaLink handle="@kshau_" icon={YouTubeLogo} href="https://youtube.com/@kshau_"/>
                 <SocialMediaLink handle="@kshau" icon={GitHubLogo} href="https://github.com/kshau"/>
             </div>
