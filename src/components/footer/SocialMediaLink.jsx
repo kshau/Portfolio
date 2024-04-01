61da316e12b407316c09ffbb5ea1ecbbb143d673
Shaurya Kumar <63877755+kshau@users.noreply.github.com>
2024-04-01 14:31:23 -0400

    Update SocialMediaLink.jsx

--- src/components/footer/SocialMediaLink.jsx
+++ src/components/footer/SocialMediaLink.jsx
@@ -4,7 +4,7 @@ function SocialMediaLink(props) {
 
     return (
         <>
-            <a className="flex flex-row text-white font-bold bg-social-link-bg w-fit h-fit rounded-[20px] p-[10px] mx-[7px] mt-[15px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300" href={href} target="_blank">
+            <a className="flex flex-row text-white font-bold bg-social-link-bg w-fit h-fit rounded-[20px] p-[10px] mx-[7px] mt-[15px] transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300" href={href} target="_blank">
                 <img src={icon} className="w-[38px]"/>
                 <span className="ml-[15px] text-[25px]">{handle}</span>
             </a>
