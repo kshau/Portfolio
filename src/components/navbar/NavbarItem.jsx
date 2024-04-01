40166f5098b7df62faf8bc86e8684f84622406a6
Shaurya Kumar <63877755+kshau@users.noreply.github.com>
2024-04-01 14:32:03 -0400

    Update NavbarItem.jsx

--- src/components/navbar/NavbarItem.jsx
+++ src/components/navbar/NavbarItem.jsx
@@ -4,7 +4,7 @@ function NavbarItem(props) {
 
     return (
         <>
-            <a href={href} className={`flex flex-row mx-[30px] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 decoration-navbar-item-underline underline-offset-[6px]`}>
+            <a href={href} className={`flex flex-row mx-[30px] transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-300 decoration-navbar-item-underline underline-offset-[6px]`}>
                 <img src={icon} className={`w-[50px] filter ${(name == active) ? ("") : ("brightness-[0.7]")}`}/>
                 <span className={`text-[30px] mt-[27px] font-bold ml-[15px] ${(name == active) ? ("text-white") : ("text-gray")}`}>{name}</span>
             </a>
